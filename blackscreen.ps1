4fc3dca0951a3e3d5584036ffcd8f41563cf9f8e
rat <112645929+iLoveRat@users.noreply.github.com>
2023-06-09 23:01:01 +0200

    fogor half the code for blackscreen
    oops

--- blackscreen.ps1
+++ blackscreen.ps1
@@ -1,4 +1,14 @@
 function turnOffScreen {
+    # Source: http://www.powershellmagazine.com/2013/07/18/pstip-how-to-switch-off-display-with-powershell/
+
+    # Turn display off by calling WindowsAPI.
+ 
+    # SendMessage(HWND_BROADCAST,WM_SYSCOMMAND, SC_MONITORPOWER, POWER_OFF)
+    # HWND_BROADCAST  0xffff
+    # WM_SYSCOMMAND   0x0112
+    # SC_MONITORPOWER 0xf170
+    # POWER_OFF       0x0002
+ 
     Add-Type -TypeDefinition '
     using System;
     using System.Runtime.InteropServices;
@@ -13,6 +23,16 @@ function turnOffScreen {
              IntPtr wParam,
              IntPtr lParam
           );
+ 
+          public static void PowerOff ()
+          {
+             SendMessage(
+                (IntPtr)0xffff, // HWND_BROADCAST
+                0x0112,         // WM_SYSCOMMAND
+                (IntPtr)0xf170, // SC_MONITORPOWER
+                (IntPtr)0x0002  // POWER_OFF
+             );
+          }
        }
     }
     '
